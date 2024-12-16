867d46b8e1dca6711077f747451bd1247793b482
Muhammad Qasim Omer <mqasimomer@gmail.com>
2024-12-16 22:45:33 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Muhammad Qasim Omer",
+  title: "Beta Mlsa",
+  email: "mqasimomer@gmail.com",
+  gitHub: "github.com/QasimOmer",
+  linkedIn: "linkedin.com/in/mqasimomer",
 };
 
 const primaryColor = "#4E567E";
